29863b9ad1af60b3e852008a40b397769acc7d9e
Boban Acimovic <boban.acimovic@gmail.com>
2020-08-11 21:56:54 +0200

    log clicks

--- frontend/src/components/VolumesList.svelte
+++ frontend/src/components/VolumesList.svelte
@@ -4,19 +4,24 @@
   import { volume } from "../store";
 
   const promise = get<k8s.V1PersistentVolume[]>("/volumes");
+  function setVolume(vol: k8s.V1PersistentVolume) {
+    console.log(vol);
+    $volume = vol;
+  }
 </script>
+
+<style>
+  li:hover {
+    cursor: pointer;
+  }
+</style>
 
 {#await promise}
   <p>loading...</p>
 {:then response}
   <ul>
     {#each response.parsedBody as vol}
-      <li
-        on:click={() => {
-          $volume = vol;
-        }}>
-        {vol.metadata.name}
-      </li>
+      <li on:click={() => setVolume(vol)}>{vol.metadata.name}</li>
     {/each}
   </ul>
 {:catch error}
