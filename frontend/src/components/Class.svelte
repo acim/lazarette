a960199b58d98bbaeb9fa278acd6f59ba55809b8
Boban Acimovic <boban.acimovic@gmail.com>
2020-08-22 12:09:04 +0200

    add class component

--- frontend/src/components/Class.svelte
+++ frontend/src/components/Class.svelte
@@ -2,6 +2,26 @@
   import type { V1StorageClass } from "@kubernetes/client-node";
 
   export let storageClass: V1StorageClass;
+
+  const isDefault = (): boolean => {
+    return (
+      storageClass.metadata.annotations.hasOwnProperty(
+        "storageclass.kubernetes.io/is-default-class"
+      ) &&
+      storageClass.metadata.annotations[
+        "storageclass.kubernetes.io/is-default-class"
+      ] === "true"
+    );
+  };
 </script>
 
-<section />
+<section>
+  <h3>{storageClass.metadata.name}</h3>
+  {#if isDefault()}
+    <p>Default</p>
+  {/if}
+  <p>Provisioner: {storageClass.provisioner}</p>
+  <p>Reclaim policy: {storageClass.reclaimPolicy}</p>
+  <p>Allow expansion: {storageClass.allowVolumeExpansion}</p>
+  <p>Binding mode: {storageClass.volumeBindingMode}</p>
+</section>
