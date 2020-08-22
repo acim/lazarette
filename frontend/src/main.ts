63e35e08577cf7190c7bcc642112ee71d462ffcf
Boban Acimovic <boban.acimovic@gmail.com>
2020-08-22 14:07:49 +0200

    refactor

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import transition from "crayon-transition";
 import animate from "crayon-animate";
 import svelte from "crayon-svelte";
 import Volumes from "./pages/Volumes.svelte";
-import Classes from "./pages/Classes.svelte";
+import StorageClasses from "./pages/StorageClasses.svelte.svelte";
 
 const target = document.getElementById("app");
 const nav = crayon.create();
@@ -21,6 +21,6 @@ nav.path("/", (req, res) => req.redirect("/volumes"));
 
 nav.path("/volumes", (req, res) => req.mount(Volumes, { req, nav }));
 
-nav.path("/classes", (req, res) => req.mount(Classes, { req, nav }));
+nav.path("/classes", (req, res) => req.mount(StorageClasses, { req, nav }));
 
 nav.load();
--- frontend/src/pages/StorageClasses.svelte
+++ frontend/src/pages/StorageClasses.svelte
@@ -1,11 +1,11 @@
 <script type="ts">
   import type crayon from "crayon";
   import Nav from "../components/Nav.svelte";
-  import ClassesList from "../components/ClassesList.svelte";
+  import StorageClassesList from "../components/StorageClassesList.svelte";
 
   export let req: crayon.Context;
   export let nav: crayon.Router;
 </script>
 
 <Nav {req} {nav} />
-<ClassesList />
+<StorageClassesList />
