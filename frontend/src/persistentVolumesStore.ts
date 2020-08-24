58af2814d2418f5d214dccdb8ef6d6a99ee79b41
Boban Acimovic <boban.acimovic@gmail.com>
2020-08-24 21:01:09 +0200

    introduce persistentVolumesStore

--- frontend/src/persistentVolumesStore.ts
+++ frontend/src/persistentVolumesStore.ts
@@ -1,18 +1,11 @@
 import { writable, Writable } from "svelte/store";
+
 import type {
-  V1StorageClass,
   V1PersistentVolume,
   V1PersistentVolumeClaim,
   V1Pod,
 } from "@kubernetes/client-node";
 import { get, HttpResponse } from "./fetch";
-
-export const volume = writable<V1PersistentVolume | null>(null);
-
-interface StorageClasses {
-  classes: V1StorageClass[];
-  error: string;
-}
 
 interface PersistentVolume {
   volume: V1PersistentVolume;
@@ -31,20 +24,6 @@ export interface PersistentVolumesWritable<T> extends Writable<T> {
    */
   load(): void;
 }
-
-export const storageClasses = writable<V1StorageClass[]>([]);
-
-export const loadStorageClasses = async () => {
-  let res: HttpResponse<StorageClasses>;
-  try {
-    res = await get<StorageClasses>("/v1/classes.json");
-    storageClasses.set(res.parsedBody.classes);
-  } catch (err) {
-    throw new Error(
-      res?.parsedBody?.error !== "" ? res.parsedBody.error : err.message
-    );
-  }
-};
 
 function createPersistentVolumes(): PersistentVolumesWritable<
   PersistentVolume[]
