c8fa6c8148d4179ab7e7f5a2c60921fdeb28ed77
Boban Acimovic <boban.acimovic@gmail.com>
2020-09-15 14:48:56 +0200

    add first unit test

---
+++ pkg/k8s/util_test.go
@@ -0,0 +1,40 @@
+package k8s
+
+import "testing"
+
+func TestJsonPointerEscape(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		out  string
+	}{
+		{
+			name: "blank strings",
+			in:   "",
+			out:  "",
+		},
+		{
+			name: "alone ~",
+			in:   "~",
+			out:  "~0",
+		},
+		{
+			name: "alone /",
+			in:   "/",
+			out:  "~1",
+		},
+		{
+			name: "mixed",
+			in:   "~a/b~~/./~s",
+			out:  "~0a~1b~0~0~1.~1~0s",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if out := jsonPointerEscape(tt.in); out != tt.out {
+				t.Errorf("got %q, want %q", out, tt.out)
+			}
+		})
+	}
+}
