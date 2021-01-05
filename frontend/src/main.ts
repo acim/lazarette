580f7dd9285629e118c7b11c729c1d15adfe125e
Boban Acimovic <boban.acimovic@gmail.com>
2021-01-05 09:39:03 +0100

    remove crayon-animate dependency

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,26 +1,26 @@
-import crayon from "crayon"
-import transition from "crayon-transition"
-import animate from "crayon-animate"
-import svelte from "crayon-svelte"
-import PersistentVolumes from "./pages/PersistentVolumes.svelte"
-import StorageClasses from "./pages/StorageClasses.svelte"
+import crayon from 'crayon'
+import transition from 'crayon-transition'
+// import animate from "crayon-animate"
+import svelte from 'crayon-svelte'
+import PersistentVolumes from './pages/PersistentVolumes.svelte'
+import StorageClasses from './pages/StorageClasses.svelte'
 
-const target = document.getElementById("app")
+const target = document.getElementById('app')
 const nav = crayon.create()
 
 nav.use(svelte.router(target))
 nav.use(transition.loader())
-nav.use(
-  animate.defaults({
-    name: transition.fade,
-    duration: 350,
-  })
-)
+// nav.use(
+//   animate.defaults({
+//     name: transition.fade,
+//     duration: 350,
+//   })
+// )
 
-nav.path("/", (req, res) => req.redirect("/volumes"))
+nav.path('/', (req, res) => req.redirect('/volumes'))
 
-nav.path("/volumes", (req, res) => req.mount(PersistentVolumes, { nav }))
+nav.path('/volumes', (req, res) => req.mount(PersistentVolumes, { nav }))
 
-nav.path("/classes", (req, res) => req.mount(StorageClasses, { nav }))
+nav.path('/classes', (req, res) => req.mount(StorageClasses, { nav }))
 
 nav.load()
